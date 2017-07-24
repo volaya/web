8bf438509de911b5b480f8b19df72b2922d6b1d6
volaya <volayaf@gmail.com>
2017-07-24 20:24:22 +0200

    update

--- texts/updatetexts.py
+++ texts/updatetexts.py
@@ -10,24 +10,26 @@ import shutil
 from subprocess import call
 
 def buildAndCopy():
-    path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),  "textos", "convert")
+    path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))),  "textos", "convert")
     buildFile = os.path.join(path, "convert.py")
-    call("python", buildFile)
+    print buildFile
+    call("python.exe " +  buildFile)
     
-    dstParent = os.path.dirname(__file__)
+    dstParent = os.path.dirname(os.path.abspath(__file__))
     src = os.path.join(path, "epub")
-    dst = os.path.join("epub")
+    dst = os.path.join(dstParent, "epub")
     if os.path.exists(dst):
-        shutil.rmtree(dst)
+        shutil.rmtree(dst, ignore_errors=True)
     shutil.copytree(src, dst)
     src = os.path.join(path, "html")
-    dst = os.path.join("html")
+    dst = os.path.join(dstParent, "html")
     if os.path.exists(dst):
-        shutil.rmtree(dst)
+        shutil.rmtree(dst, ignore_errors=True)
     shutil.copytree(src, dst)    
 
-    call("git", "commit", "-a", "-m", "updated site")
-    call("git", "push")    
+    call("git add .")
+    call("git commit -m update")
+    call("git push")    
 
 if __name__ == '__main__':  
     buildAndCopy()
