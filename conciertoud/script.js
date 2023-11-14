8b1cb53da8e33d17dab90cc029b1a0a6dbf95f0e
volaya <volayaf@gmail.com>
2023-11-14 14:01:57 +0100

    añadido concierto

---
+++ conciertoud/script.js
@@ -0,0 +1,11 @@
+
+// fullpage customization
+$('#fullpage').fullpage({
+  sectionsColor: ['#FFFFFF', '#FFFFFF', '#FFFFFF'],
+  sectionSelector: '.vertical-scrolling',
+  slideSelector: '.horizontal-scrolling',
+  navigation: true,
+  slidesNavigation: true,
+  controlArrows: false, 
+
+});
